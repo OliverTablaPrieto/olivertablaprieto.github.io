a87eabcc2e2ccedaca51e19dd34b8108b7520160
OliverTablaPrieto <olivertabla10@gmail.com>
2026-02-22 01:27:22 +0100

    Missatge descriptiu del canvi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
   site: 'https://olivertablaprieto.github.io',
   base: '/',
-  // Solo forzamos barras finales en producción (GitHub)
-  trailingSlash: isProd ? 'always' : 'ignore', 
-  build: {
-    // Solo generamos estructura de directorios en producción
-    format: isProd ? 'directory' : 'file',
-  }
+  outDir: 'dist',
 });
